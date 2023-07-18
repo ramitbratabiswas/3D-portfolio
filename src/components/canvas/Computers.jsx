ef67d82064d2cabba34a83d5d56db63619cbf056
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2023-07-18 11:15:25 -0700

    completed small devices ComputersCanvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -19,6 +19,23 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+      const mediaQuery = window.matchMedia('max-width: 768px');
+      setIsMobile(mediaQuery.matches);
+
+      const handleMediaQueryChange = (event) => {
+        setIsMobile(event.matches);
+      }
+
+      mediaQuery.addEventListener('change', handleMediaQueryChange);
+      return () => {
+        mediaQuery.removeEventListener('change', handleMediaQueryChange);
+      }
+    }, []);
+
   return (
     <Canvas
       frameloop='demand'
