654c5b0d73aef12f188a0d5e51ee5c4b8abd7e08
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2023-07-18 20:07:56 -0700

    created SectionWrapper

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion.js';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div 
+        variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div 
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 
+                    min-h-[280px] flex justify-evenly
+                    items-center flex-col'
+        >
+          <img src={icon} alt={title}
+            className='w-16 h-16 object-contain'/>
+          <h3 className='text-white text-center font-bold text-[20px]'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Obcaecati deleniti eligendi facilis recusandae natus 
+        provident maiores quia aliquam cumque libero, optio adipisci
+        eveniet ipsam, illo dolore voluptatum in nobis. Asperiores. 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Ducimus, illo. Nulla saepe molestias labore deleniti. Enim, 
+        blanditiis quo! Dolor nesciunt soluta optio ipsam non cupiditate 
+        illo aspernatur porro praesentium nostrum!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
   )
 }
 
